325c5f1b7f1971a16c9fb4399b0d5e2f8c729c32
Mostafa <msari808755@gmail.com>
2025-11-16 00:17:54 -0800

    AboutMePageBetaStyling

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {motion} from "framer-motion";
-
-
+import "../../styles/AboutMe/AboutMe.css";
+import MyImg from "../../assets/HomeLogo.jpg";
 
 const AboutMe = () =>{
     return(
@@ -9,12 +9,17 @@ const AboutMe = () =>{
             <div className="AboutMeWrapper">
                 <div className="AboutContainer">
                     <div className="AboutMeImg">
-                        <img src="#" alt="AboutMe"/>
+                        <img src={MyImg} alt="AboutMe"/>
                     </div>
                     <div className="AboutMeText">
                         <h1>About Me</h1>
                         <hr/>
-                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum quidem esse architecto praesentium,
+                        <p>
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum quidem esse architecto praesentium,
+                            consectetur suscipit iure dolore blanditiis tempora aspernatur, natus totam! Corrupti voluptas, deserunt optio adipisci saepe libero! Repudiandae?
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum quidem esse architecto praesentium,
+                            consectetur suscipit iure dolore blanditiis tempora aspernatur, natus totam! Corrupti voluptas, deserunt optio adipisci saepe libero! Repudiandae?
+                            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Rerum quidem esse architecto praesentium,
                             consectetur suscipit iure dolore blanditiis tempora aspernatur, natus totam! Corrupti voluptas, deserunt optio adipisci saepe libero! Repudiandae?
                         </p>
                     </div>
